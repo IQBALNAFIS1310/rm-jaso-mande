c2039f5d8f19ef7715d0581500bc4c76a68b8a5e
IQBALNAFIS1310 <iqbalnafis487@gmail.com>
2025-09-08 22:33:23 +0700

    halo

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const apiKey = localStorage.getItem("informasiakun");
+  if (!apiKey) {
+    return <Navigate to="/signin" replace />;
+  }
+  return children;
+}
